19e66ee6003752a40fed264bb21146863887a65e
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-27 00:53:56 +0100

    Added logo to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#0b1215] text-white'>
+        <div className="">
+            <div className="">
+                Teemah
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
