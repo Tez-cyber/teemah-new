69a83ed0cb4516affb59a9bce7e2f709eb86d009
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-27 01:05:17 +0100

    Navbar completed for large screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
 const Navbar = () => {
+    const navLinks = [
+        { name: "home", url: "#"},
+        { name: "about", url: "#"},
+        { name: "our menu", url: "#"},
+        { name: "Contact us", url: "#"},
+    ]
   return (
     <div className='bg-[#0b1215] text-white'>
-        <div className="">
-            <div className="">
+        <div className=" w-[1200px] mx-auto flex justify-between items-center p-6">
+            <div className="text-3xl  xl font-bold">
                 Teemah
             </div>
-            <div className="">
-
+            <div className="flex gap-4 capitalize ">
+                {
+                    navLinks.map((link, i) => (
+                        <a href={link.url} className="text-xl">
+                            {link.name}
+                        </a>
+                    ))
+                }
             </div>
         </div>
     </div>
