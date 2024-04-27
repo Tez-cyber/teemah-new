a43b06c77bc7b502c20fcf6e4df3d37c9b920ab5
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-27 01:27:30 +0100

    Added hover effect for nav-items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,15 +8,16 @@ const Navbar = () => {
         { name: "Contact us", url: "#"},
     ]
   return (
-    <div className='bg-[#0b1215] text-white'>
-        <div className=" w-[1200px] mx-auto flex justify-between items-center p-6">
+    <div className='bg-darkHueBg text-white'>
+        <div className=" w-contain mx-auto flex justify-between items-center p-6">
             <div className="text-3xl  xl font-bold">
                 Teemah
             </div>
-            <div className="flex gap-4 capitalize ">
+            {/* ---------Nav for large screen */}
+            <div className="flex gap-5 capitalize ">
                 {
                     navLinks.map((link, i) => (
-                        <a href={link.url} className="text-xl">
+                        <a href={link.url} className=" ml-5 text-xl pb-2 after:content-[''] after:block after:h-[2px] after:w-[3px] after:bg-inherit after:transition-all after:duration-500 after:hover:w-full after:hover:bg-[#eaa023]">
                             {link.name}
                         </a>
                     ))
