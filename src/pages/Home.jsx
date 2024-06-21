28d4ef539a4d043aa57e3cbd67c5c06947342296
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-21 20:34:57 +0100

    installed react-icons and created Works.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Jumbotron from '../components/HomePage/Jumbotron'
 import AboutSection from '../components/HomePage/AboutSection'
+import Works from '../components/HomePage/Works'
 
 const Home = () => {
   return (
     <div>
       <Jumbotron />
       <AboutSection />
+      <Works />
     </div>
   )
 }
