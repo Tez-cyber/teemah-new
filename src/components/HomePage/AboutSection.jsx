50299305ca401c26d4c7da6e9727d21718d582ec
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-22 11:22:45 +0100

    Added section heading to About

--- src/components/HomePage/AboutSection.jsx
+++ src/components/HomePage/AboutSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const AboutSection = () => {
   return (
     <div className='bg-[#16161d] h-screen text-white py-5'>
-      <div className="">
+      <div className="row">
         <div className="section-title">
-          <h2 data-title="our story">About Us</h2>
+          <h2 data-title="our story">about us</h2>
         </div>
       </div>
     </div>
