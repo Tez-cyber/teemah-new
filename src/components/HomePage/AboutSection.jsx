4cb42229f4b5955cd91daaa23d46f36df5a15a43
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-22 11:04:41 +0100

    Added tailwind utilities to index.css

--- src/components/HomePage/AboutSection.jsx
+++ src/components/HomePage/AboutSection.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div>
+    <div className='bg-[#16161d] h-screen text-white py-5'>
       <div className="">
-        <div className="">
-          
+        <div className="section-title">
+          <h2 data-title="our story">About Us</h2>
         </div>
       </div>
     </div>
