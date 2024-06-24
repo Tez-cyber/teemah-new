3ad9bf6c4c61322c5efed10bb51c0c3cb808bbb0
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-24 23:31:37 +0100

    Completed works section

--- src/components/HomePage/Works.jsx
+++ src/components/HomePage/Works.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { FaTruck, FaCookieBite } from 'react-icons/fa'
+import { BiFoodMenu } from 'react-icons/bi'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div className='bg-black text-white pt-10 pb-20'>
+      {/*------------- Heading ------------- */}
+      <div className="row">
+        <div className="section-title">
+          <h2 data-title="" className='text-white'>How it works</h2>
+        </div>
+      </div>
+      {/*----------------------- Content ------------------------- */}
+      <div className="row gap-10 px-10 lg:w-contain lg:mx-auto lg:gap-4">
+        <div className="">
+          {/*------ Works-head------ */}
+          <div className="flex gap-3 items-center pb-3 text-mainColor text-2xl">
+            <i className="text-3xl"><BiFoodMenu /></i>
+            <h5 className="">Place order</h5>
+          </div>
+          {/*------ Works-content------ */}
+          <p className='worksContent'>
+            Select from our wide variety of signature dishes,
+            custom, or specialty meal options. Dishes prepared
+            with highest quality ingredient to deliver utmost
+            satisfaction
+
+          </p>
+        </div>
+        <div className="">
+          {/*------ Works-head------ */}
+          <div className="flex gap-3 items-center pb-3 text-mainColor text-2xl">
+            <i className="text-3xl"><FaCookieBite /></i>
+            <h5 className="">Cook</h5>
+          </div>
+          {/*------ Works-content------ */}
+          <p className='worksContent'>
+            Our staffs will carefully prepare, cook and package
+            all your meals and have them fresh and ready to go !
+          </p>
+        </div>
+        <div className="">
+          {/*------ Works-head------ */}
+          <div className="flex gap-3 items-center pb-3 text-mainColor text-2xl">
+            <i className="text-3xl"><FaTruck /></i>
+            <h5 className="">Pickup / Delivery</h5>
+          </div>
+          {/*------ Works-content------ */}
+          <p className='worksContent'>
+            You get to choose to pick up your meals
+            in our very own location or even choose a
+            delivery method at your convinience.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
