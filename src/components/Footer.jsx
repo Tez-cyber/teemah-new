3d1a41606d73c117268ab3e8b99842c3931b2664
Salami Azeez <salamitopeazeez@gmail.com>
2024-07-22 08:39:32 +0100

    Added comment for footerItem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,26 @@ const Footer = () => {
         <footer className="footer relative z-10 text-gray-400 before:-z-10 before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-black/70" id="footer">
             <div className=" ">
                 <div className="row justify-center gap-8 items-center px-10 py-20 ">
-                    <div className="w-1/3 py-0 px-4 text-center footer-item">
+                    {/* ====footer item==== */}
+                    <div className="footerItem">
                         <h3 className='capitalize text-xl font-bold'>Our Location</h3>
                         <p>Gbokoniyi, lecturer bus stop,
                             <br />Abeokuta
                         </p>
                     </div>
-                    <div className="w-1/3 py-0 px-4 text-center footer-item">
+                    {/* ====footer item==== */}
+                    <div className="footerItem ">
                         <h3 className='capitalize text-xl font-bold'>Opening Hours</h3>
                         <p>Monday to Sunday
                             <br />9:00AM - 10PM
                         </p>
                     </div>
-                    <div className="w-1/3 py-0 px-4 text-center footer-item">
+                    {/* ====footer item==== */}
+                    <div className="footerItem">
                         <h3 className='capitalize text-xl font-bold'>Contact Us</h3>
                         <p>+2347059269642</p>
                         <p>bisolatiamiyu3@gmail.com</p>
-                        <div className="flex items-center justify-center gap-4 pt-4 text-2xl social-links">
+                        <div className="flex items-center justify-center gap-4 pt-4 text-2xl">
                             <a
                                 target="_blank"
                                 rel="noreferrer"
